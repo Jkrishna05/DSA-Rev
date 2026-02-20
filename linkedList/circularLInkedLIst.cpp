108dcc167ce7266e6b533ea5028200453ceb1c6d
Jaykrishna Rout <gourimanirout@gmail.com>
2026-02-20 11:41:53 +0530

    circularLL

---
+++ linkedList/circularLInkedLIst.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+int main(){
+    
+    return 0;
+}
